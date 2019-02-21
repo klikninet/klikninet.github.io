49fe5ee54bea90f5958dabf85fcc98503330d3ef
klikninet <46600493+klikninet@users.noreply.github.com>
2019-02-21 22:39:39 +0300

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,145 @@
+<?php require_once("res/x5engine.php"); ?><!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - WEBSITE X5 UNREGISTERED VERSION 12.0.5.22 - klikninet</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Professional 12.0.5.22 - UNREGISTERED VERSION - www.websitex5.com" />
+		<meta property="og:locale" content="ru-RU" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://klikninet.ru/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="klikninet" />
+		<meta property="og:image" content="http://klikninet.ru/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="60">
+		<meta property="og:image:height" content="60">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="favicon.ico?12-0-5-22-636863846482735283" type="image/vnd.microsoft.icon" />
+		<link rel="stylesheet" type="text/css" href="style/reset.css?12-0-5-22" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?12-0-5-22" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?12-0-5-22" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?12-0-5-22" media="screen" />
+		<link rel="stylesheet" type="text/css" href="style/menu.css?12-0-5-22" media="screen" />
+		<!--[if lte IE 7]><link rel="stylesheet" type="text/css" href="style/ie.css?12-0-5-22" media="screen" /><![endif]-->
+		<link rel="alternate" type="application/rss+xml" title="Новости" href="x5feed.php" />
+		
+		<script type="text/javascript" src="res/jquery.js?12-0-5-22"></script>
+		<script type="text/javascript" src="res/x5engine.js?12-0-5-22" data-files-version="12-0-5-22"></script>
+		
+		
+		
+		<script type="text/javascript">
+			x5engine.boot.push(function () { x5engine.utils.checkBrowserCompatibility(); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imHeaderBg"></div>
+		<div id="imFooterBg"></div>
+		<div id="imPage">
+			<div id="imHeader">
+				<h1 class="imHidden">Поиск - WEBSITE X5 UNREGISTERED VERSION 12.0.5.22 - klikninet</h1>
+				
+				<div class="imTemplateContent" style="position: absolute; top: 27px; left: 885px; width: 187px; height: 56px; overflow: hidden;"><a href="https://time.is/Moscow" id="time_is_link" rel="nofollow" style="font-size:25px;color:#000000;background:00ffffff">Москва</a>
+
+<span id="Moscow_z71d" style="font-size:25px;color:#000000;background:00ffffff"></span>
+
+<script src="//widget.time.is/ru.js"></script>
+
+<script>
+
+time_is_widget.init({Moscow_z71d:{template:"TIME<br>DATE", date_format:"dayname daynum.monthnum.yy"}});
+
+</script></div>
+			</div>
+			<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+			<a id="imGoToMenu"></a><p class="imHidden">Главное меню:</p>
+			<div id="imMnMnGraphics"></div>
+			<div id="imMnMn" class="auto">
+				<div class="hamburger-site-background menu-mobile-hidden"></div><div class="hamburger-button"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container"><div class="hamburger-menu-background menu-mobile-hidden"><div class="hamburger-menu-close-button"><span>&times;</span></div></div></div>
+				<span itemscope itemtype="http://schema.org/Attorney"><a itemprop="url" href="index.html"><img src="style/responsive-logo.png" itemprop="logo" class="menu-mobile-logo" alt="klikninet"/></a>
+				</span><ul class="auto menu-mobile-hidden">
+					<li id="imMnMnNode0" class=" imPage">
+						<a href="index.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Главная</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode5" class=" imPage">
+						<a href="wallet.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Кошельки</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode8" class=" imPage">
+						<a href="obmen.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Обменники</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode7" class=" imPage">
+						<a href="exch.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Биржи</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode9" class=" imPage">
+						<a href="crypta.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Крипта</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode10" class=" imPage">
+						<a href="miner.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Майнинг</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode4" class=" imPage">
+						<a href="credit.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Кредит</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode6" class=" imPage">
+						<a href="kran.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Краны</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode3" class=" imPage">
+						<a href="bux.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Буксы</span></span>
+							</span>
+						</a>
+					</li></ul>
+			</div>
+			<div id="imContentGraphics"></div>
+			<div id="imContent">
+				<a id="imGoToCont"></a>
+				<h2 id="imPgTitle">Результаты поиска</h2><?php
+$search = new imSearch();
+$keys = isset($_GET['search']) ? $_GET['search'] : "";
+$page = isset($_GET['page']) ? $_GET['page'] : 0;
+$type = isset($_GET['type']) ? $_GET['type'] : "pages"; ?>
+<div class="searchPageContainer">
+<?php echo $search->search($keys, $page, $type); ?>
+</div>
+
+				<div class="imClear"></div>
+			</div>
+			<div id="imFooter">
+				
+				<div class="imTemplateContent" onclick=" return x5engine.utils.imPopUpWin('https://www.youtube.com/channel/UC8i_KLcL7R8V3Sh50ksFr1A?view_as=subscriber', '', -1, -1, true);" onmouseover="$('#imHoverImage_3').css('display','')" style="position: absolute; top: 23px; left: 13px; width: 42px; height: 42px; cursor: pointer;"><img id="imHoverImage_3" src="images/youe.png" alt="" onmouseout="$(this).css('display','none');" style="display: none; width: 42px; height: 42px;" /></div>
+				<div class="imTemplateContent" onclick=" return x5engine.utils.imPopUpWin('http://vk.com/klikninet', '', -1, -1, true);" onmouseover="$('#imHoverImage_4').css('display','')" style="position: absolute; top: 23px; left: 62px; width: 42px; height: 42px; cursor: pointer;"><img id="imHoverImage_4" src="images/vke.png" alt="" onmouseout="$(this).css('display','none');" style="display: none; width: 42px; height: 42px;" /></div>
+				<div class="imTemplateContent" onclick="x5engine.utils.location('http://klikninet.ru'); return false;" style="position: absolute; top: 15px; left: 1002px; width: 58px; height: 55px; cursor: pointer;"></div>
+				<div id="imFooterResponsiveContent"></div>
+			</div>
+		</div>
+		<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a> | <a href="#imGoToMenu" title="Прочесть этот сайт заново">Назад к главному меню</a></span>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
---
+++ x5feed.php
@@ -0,0 +1,23 @@
+<?php include('res/x5engine.php'); header('Content-Type: text/xml'); echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<atom:link href="http://klikninet.ru/x5feed.php" rel="self" type="application/rss+xml" />
+		<title><![CDATA[Новости]]></title>
+		<link>http://klikninet.ru</link>
+		<description><![CDATA[Браузерный майнинг]]></description>
+		<language>RU</language>
+		<lastBuildDate><?php echo getLastAvailableDate(array(1550233500)); ?></lastBuildDate>
+		<generator>Incomedia WebSite X5 Professional</generator>
+<?php if (time() > 1550233500): ?>
+		<item>
+			<title><![CDATA[Майнинг]]></title>
+			<link>http://klikninet.ru/miner.html</link>
+			<description><![CDATA[<div id="imRSSArticle_ag4ws6lo"><div><img class="image-0" src="http://klikninet.ru/images/jse.png"  title="" alt=""/><a href="https://platform.jsecoin.com/?lander=2&utm_source=referral&utm_campaign=aff144693&utm_content=" target="_blank">Браузерный майнинг JSE</a></div></div>]]></description>
+			<pubDate>Fri, 15 Feb 2019 12:25:00 GMT</pubDate>
+			<category>imrssfeed</category>
+			<enclosure url="http://klikninet.ru/images/jse_22gf3wrz.png" length="7420" type="image/png" />
+			<guid isPermaLink="false">http://klikninet.ru/rss/ag4ws6lo</guid>
+		</item>
+<?php endif; ?>
+	</channel>
+</rss>
